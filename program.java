f8f56f277354def34186d06823eaaa324321eb5b
juuls04 <97850777+juuls04@users.noreply.github.com>
2023-09-04 12:06:12 +0000

    multiplier

--- program.java
+++ program.java
@@ -1,11 +1,23 @@
 public class program {
 
-    public program() {
+    int nummer;
 
+    public program(int input) {
+        nummer = input;
+    }
+
+    public int multiply(int mult) {
+
+        int number;
+        number = nummer * mult;
+        return number;
     }
 
     public static void main(String[] args) {
         System.out.println("hi");
+        program programma = new program(5);
+        System.out.println(programma.nummer);
+        System.out.println(programma.multiply(8));
     }
 
 }
